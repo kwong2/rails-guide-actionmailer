a192fb1da1455178673d214756bf54e532c075af
Kevin Wong <k.wong4@yahoo.com>
2017-07-04 17:12:09 -0400

    corrected error in index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,20 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        #Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver_later
+
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new'}
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # GET /users/1
